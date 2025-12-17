c69f01ee629f8eac41c918a6f7c18f8beb3b2892
rashmigmr13-eng <rashmigmr13@gmail.com>
2025-12-17 11:29:34 +0530

    Update output.tf

--- test-eks/output.tf
+++ test-eks/output.tf
@@ -1,15 +1,7 @@
-output "cluster_id" {
-  value = aws_eks_cluster.microdegree.id
-}
-
-output "node_group_id" {
-  value = aws_eks_node_group.microdegree.id
-}
-
 output "vpc_id" {
-  value = data.aws_vpc.default.id
+  value = aws_vpc.microdegree_vpc.id
 }
 
 output "subnet_ids" {
-  value = data.aws_subnets.default.ids
+  value = aws_subnet.microdegree_subnet[*].id
 }
